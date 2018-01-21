4581f3476313f611b298191b099afbf069c02be4
AoXiaole <1544915738@qq.com>
2018-01-22 00:21:58 +0800

    提交json代码

--- c_c++/application/freeswitch_work/mod_as_presence_json_v2.cpp
+++ c_c++/application/freeswitch_work/mod_as_presence_json_v2.cpp
@@ -1,8 +1,17 @@
 #include"mod_as_presence_json_v2.h"
+
+const char * CJsonMapNd::m_pTypeString[cJSON_TypeMAX] = {
+	"cJSON_Empty",
+	"cJSON_Object",
+	"cJSON_String",
+	"cJSON_Number",
+	"cJSON_Array"
+	};
+
 CJsonMapNd::CJsonMapNd(const char *pStr)
 {
     m_nType = cJSON_String; 
-    Data.m_pStr = pStr; 
+    Data.m_pStr = new string(pStr); 
 }
 
 CJsonMapNd::CJsonMapNd(int nInt)
@@ -11,54 +20,199 @@ CJsonMapNd::CJsonMapNd(int nInt)
     Data.m_int = nInt; 
 }
 
+CJsonMapNd::CJsonMapNd(CJsonMapNd & JsonMapNd)
+{
+	Filling(JsonMapNd);
+}
+
 CJsonMapNd:: CJsonMapNd()
 {
-    m_nType = cJSON_Object;
+    m_nType = cJSON_Empty;
 }
 
 CJsonMapNd::~CJsonMapNd()
 {
-    if(cJSON_Object == m_nType)
-    {
-        if(Data.m_pMap)
-        {
-            map<string, CJsonMapNd *>::iterator it = Data.m_pMap->begin();
-            for(; it != Data.m_pMap->end(); it++)
-            {
-                if(it->second)
-                {
-                    delete it->second;
-                }
-            }
-            delete Data.m_pMap;
-        }
-    }
-    else if(cJSON_Array == m_nType)
-    {
-        if(Data.ARRAY.pArrayObj)
-        {
-            delete [] Data.ARRAY.pArrayObj;
-        }
-    }
+    Clear();
 }
 
+void CJsonMapNd::Clear()
+{
+	if(cJSON_Object == m_nType)
+	{
+		if(Data.m_pMap)
+		{
+			map<string, CJsonMapNd *>::iterator it = Data.m_pMap->begin();
+			for(; it != Data.m_pMap->end(); it++)
+			{
+				if(it->second)
+				{
+					delete it->second;
+				}
+			}
+			delete Data.m_pMap;
+		}
+	}
+	else if(cJSON_Array == m_nType)
+	{
+		int size = Data.m_pList->size();
+		for(int i = 0; i < size; i++)
+		{
+			if(NULL != (*Data.m_pList)[i])
+			{
+				delete (*Data.m_pList)[i];
+			}
+		}
+			
+		delete Data.m_pList;
+	}
+	else if(cJSON_String == m_nType)
+	{
+		delete Data.m_pStr;
+	}
+
+}
+
+void CJsonMapNd::Filling(CJsonMapNd &JsonMapNd)
+{	
+	m_nType = JsonMapNd.m_nType;
+	switch(m_nType)
+	{
+		case cJSON_Number:
+		{
+			Data.m_int = JsonMapNd.Data.m_int;
+			break;
+		}
+		case cJSON_String:
+		{
+			Data.m_pStr = new string(*(JsonMapNd.Data.m_pStr));
+			break;
+		}
+		case cJSON_Object:
+		{
+			Data.m_pMap = new  map<string, CJsonMapNd *>(*(JsonMapNd.Data.m_pMap));
+			map<string, CJsonMapNd *>::iterator it = Data.m_pMap->begin();
+			for(; it != Data.m_pMap->end(); it++)
+			{
+				if(it->second)
+				{
+					it->second = new CJsonMapNd(*(it->second));
+				}
+			}
+			break;
+		} 
+		case cJSON_Array:
+		{
+			Data.m_pList = new vector<CJsonMapNd *>(*(JsonMapNd.Data.m_pList));
+			int size = Data.m_pList->size();
+			for(int i = 0; i < size; i++)
+			{
+				if(NULL != (*Data.m_pList)[i])
+				{
+					(*Data.m_pList)[i] = new CJsonMapNd(*((*Data.m_pList)[i]));
+				}
+			}
+			break;
+		}
+		case cJSON_Empty:
+		{
+			break;
+		}
+		default:
+		{
+			break;
+		}
+			
+	}
+	
+}
+
+
+switch_status_t CJsonMapNd::EncodeJsonString(const char* pString)
+{
+
+	cJSON *m_pjson;	
+	switch_status_t nRet = SWITCH_STATUS_FALSE;
+	if(NULL == pString || NULL == (m_pjson = cJSON_Parse(pString))) 
+	{
+	    return SWITCH_STATUS_FALSE;
+	}
+
+	nRet = EncodeJson(m_pjson);
+	cJSON_Delete(m_pjson);
+	
+	return nRet;
+}
+
+
+bool CJsonMapNd::IsExist(const char *pKey)
+{
+	bool bRet = false;
+	
+    if(this != NULL && cJSON_Object == m_nType)
+    {
+	    map<string, CJsonMapNd *>::iterator it = Data.m_pMap->find(pKey);
+	    if( it != Data.m_pMap->end())
+	    {
+	        bRet = true;
+	    }
+    } 
+	return bRet;
+}
+
+/*
 bool CJsonMapNd::IsExist()
 {
+	bool bRet = false;
+	
     if(this != NULL)
     {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+		bRet = true;
+    } 
+	return bRet;
+}*/
+
+int CJsonMapNd::Type()
+{
+	return m_nType;
+}
+
+const char * CJsonMapNd::TypeString()
+{
+	return m_pTypeString[m_nType];
+}
+int CJsonMapNd::Size()
+{
+	//return Data.ARRAY.nCount;
+	if(cJSON_Array == m_nType)
+	{
+		return Data.m_pList->size();
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+void CJsonMapNd::SetSize(int nNums)
+{
+	//return Data.ARRAY.nCount;
+	if(cJSON_Array == m_nType)
+	{
+		Data.m_pList->resize(nNums);
+	}
+	else if(cJSON_Empty == m_nType)
+	{
+		m_nType = cJSON_Array; 
+		Data.m_pList = new vector<CJsonMapNd *>(nNums, NULL);
+	}
+		
 }
 
 const char *CJsonMapNd::Str()
 {
     if(this && cJSON_String == m_nType) 
     {
-        return Data.m_pStr;
+        return Data.m_pStr->c_str();
     }
     else
     {
@@ -96,11 +250,15 @@ switch_status_t CJsonMapNd::EncodeJson(cJSON *pJson)
     {
         cJSON *c = pJson->child; 
         int i = 0;
-        Data.ARRAY.nCount = cJSON_GetArraySize(pJson);
-        Data.ARRAY.pArrayObj = new CJsonMapNd[Data.ARRAY.nCount]();
-        while(c && (i < Data.ARRAY.nCount))
+		int nCount = cJSON_GetArraySize(pJson);
+       // Data.ARRAY.nCount = cJSON_GetArraySize(pJson);
+        //Data.ARRAY.pArrayObj = new CJsonMapNd[Data.ARRAY.nCount]();
+		Data.m_pList = new vector<CJsonMapNd *>(nCount, NULL);
+        while(c && (i < nCount))
         {
-            Data.ARRAY.pArrayObj[i].EncodeJson(c);
+           // Data.ARRAY.pArrayObj[i].EncodeJson(c);
+           	(*Data.m_pList)[i] = new CJsonMapNd();
+		   	(*Data.m_pList)[i]->EncodeJson(c);
             i++;
             c = c->next;
         }
@@ -116,7 +274,7 @@ switch_status_t CJsonMapNd::EncodeJson(cJSON *pJson)
             switch(c->type)
             {
                 case cJSON_Object:
-                    pChildJson = new CJsonMapNd(cJSON_Object);
+                    pChildJson = new CJsonMapNd();
                     nRet = pChildJson->EncodeJson(c);
                     break;
                     
@@ -152,6 +310,9 @@ switch_status_t CJsonMapNd::EncodeJson(cJSON *pJson)
 
 CJsonMapNd& CJsonMapNd::operator [](const char *pKey)
 {
+
+	CJsonMapNd& jsonmapNd = *(CJsonMapNd *)NULL;
+	
     if( NULL == this || NULL == pKey)
     {
         switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "CJsonMapNd::operator [](const char *pKey), this = %p , pKey = %p\n", this , pKey);
@@ -163,56 +324,135 @@ CJsonMapNd& CJsonMapNd::operator [](const char *pKey)
         map<string, CJsonMapNd *>::iterator it = Data.m_pMap->find(pKey);
         if( it != Data.m_pMap->end())
         {
-            return *(it->second);
+            jsonmapNd = *(it->second);
         }
         else
         {
-            switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, " CJsonMapNd::operator [](const char *pKey), pKey = %s,find = NULL\n", pKey);
-            return *(CJsonMapNd *)NULL;
+			CJsonMapNd *pChildJson = new CJsonMapNd();
+			(*Data.m_pMap)[pKey] = pChildJson;            
+            jsonmapNd = *pChildJson;
         }
     }
+	else if(cJSON_Empty == m_nType)
+	{
+		CJsonMapNd *pChildJson = new CJsonMapNd();
+		m_nType = cJSON_Object;
+		Data.m_pMap = new map<string, CJsonMapNd *>();
+		
+		(*Data.m_pMap)[pKey] = pChildJson;
+		jsonmapNd = *pChildJson;
+		
+	}
     else
     {
-        switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "CJsonMapNd::operator [](const char *pKey)  pkey = %s is not JsonObj\n", pKey);
-        return *(CJsonMapNd *)NULL;
+        switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "CJsonMapNd::operator [](const char *pKey)  pkey = %s ,type =  %s\n", pKey, TypeString);
+        jsonmapNd = *(CJsonMapNd *)NULL;
     }
+	
+	return jsonmapNd;
 }
 
 CJsonMapNd& CJsonMapNd::operator [](int nIndex)
 {
+	int nListSize = 0;
+	CJsonMapNd& jsonmapNd = *(CJsonMapNd *)NULL;
+	
     if(this == NULL )
     {
         switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "CJsonMapNd::operator [](int nIndex) this == NULL\n");
         return *(CJsonMapNd *)NULL;
     }
     
-    if(cJSON_Array == m_nType && nIndex < Data.ARRAY.nCount)
+    if(cJSON_Array == m_nType)		
     {
-        return Data.ARRAY.pArrayObj[nIndex];
+    	if(nIndex >= Data.m_pList->size())
+        {
+        	Data.m_pList->resize(nIndex + 1);
+    	}
+		if(NULL == (*Data.m_pList)[nIndex])
+		{
+			(*Data.m_pList)[nIndex] =  new CJsonMapNd();
+		}
+		jsonmapNd =  *((*Data.m_pList)[nIndex]);
     }
-    else
+	else if(cJSON_Empty == m_nType)
+	{
+		m_nType = cJSON_Array; 
+		Data.m_pList = new vector<CJsonMapNd *>(nIndex + 1, NULL);
+		(*Data.m_pList)[nIndex] =  new CJsonMapNd();
+		jsonmapNd = *((*Data.m_pList)[nIndex]);
+	}
+	else
     {
-        switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "CJsonMapNd::operator [](int nIndex) type = %d , index = %d, nCount = %d \n",m_nType, nIndex ,Data.ARRAY.nCount );
-        return *(CJsonMapNd *)NULL;
+        switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "CJsonMapNd::operator [](int nIndex) type = %s , index = %d\n", TypeString, nIndex,  cJSON_Array == m_nType ? Data.m_pList->size() :0);
+        jsonmapNd = *(CJsonMapNd *)NULL;
     }
+	return jsonmapNd;
 }
+
+
+CJsonMapNd& CJsonMapNd::operator =(const char * pStr)
+{
+	if(cJSON_String == m_nType)
+	{
+		(*Data.m_pStr) = pStr;
+	}
+	else if(cJSON_Empty == m_nType)
+	{
+		m_nType = cJSON_String;
+		Data.m_pStr = new string(pStr);
+	}
+	else
+	{
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "CJsonMapNd::operator =(const char * pStr) pStr = %s, type = %s \n", pStr, TypeString);
+	}
+	
+	return *this;	
+}
+CJsonMapNd& CJsonMapNd::operator =(int n_int)
+{
+	if(cJSON_Number == m_nType)
+	{
+		Data.m_int = n_int;
+	}
+	else if(cJSON_Empty == m_nType)
+	{
+		m_nType = cJSON_Number;
+		Data.m_int = n_int;
+	}
+	else
+	{
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "CJsonMapNd::operator =(int n_int) n_int = %d, type = %s \n", n_int, TypeString);
+	}
+	
+	return *this;
+}
+CJsonMapNd& CJsonMapNd::operator =(CJsonMapNd & jsonMapNd)
+{
+	Clear();
+	Filling(jsonMapNd);
+	
+	return *this;
+}
+
+
 
 CJsonMap::~CJsonMap()
 {
-    if(pjson)
+    if(m_pjson)
     {
-         cJSON_Delete(pjson);
+         cJSON_Delete(m_pjson);
     }
 }
 
 switch_status_t CJsonMap::EncodeJsonString(const char* pString)
 {
-  if(NULL == string || NULL == (m_pjson = cJSON_Parse(pString))) 
+  if(NULL == pString || NULL == (m_pjson = cJSON_Parse(pString))) 
   {
         return SWITCH_STATUS_FALSE;
   }
   
-  return JsonBase.EncodeJson(m_pjson);
+  return m_jsonmapNode.EncodeJson(m_pjson);
 }
 
 CJsonMapNd& CJsonMap::operator [](const char *pKey)
@@ -223,4 +463,17 @@ CJsonMapNd& CJsonMap::operator [](const char *pKey)
     }
     return m_jsonmapNode[pKey];
 }
+
+bool CJsonMap::IsExist(const char *pKey)
+{
+	return m_jsonmapNode.IsExist(pKey);
+}
+
+/*
+bool CJsonMap::IsExist()
+{
+	return true;
+}*/
+
+
 
--- c_c++/application/freeswitch_work/mod_as_presence_json_v2.h
+++ c_c++/application/freeswitch_work/mod_as_presence_json_v2.h
@@ -1,33 +1,81 @@
 #ifndef __MOD_AS_PRESENCE_JSONV2_H__
 #define __MOD_AS_PRESENCE_JSONV2_H__
 
-#include <switch.h>
-#include <switch_json.h>
+//#include <switch.h>
+//#include <switch_json.h>
 #include <string>
 #include <map>
-using namespace std;
+#include <vector>
 
+using namespace std;
+// test define
+	enum switch_status_t
+	{
+		SWITCH_STATUS_SUCCESS,
+		SWITCH_STATUS_FALSE	
+	};
+
+enum CJSONTYPE
+{
+	cJSON_Empty,
+	cJSON_Object,
+	cJSON_String,
+	cJSON_Number,
+	cJSON_Array,
+	cJSON_TypeMAX,
+
+};
+struct cJSON
+{
+	cJSON *next;
+	cJSON *child;
+	int type;
+	int valueint;
+	const char *valuestring;
+	const char *string;
+};
+
+#define switch_log_printf(argc,...)
+#define SWITCH_CHANNEL_LOG
+#define SWITCH_LOG_ERROR
+extern int cJSON_GetArraySize(cJSON *json);
+extern void cJSON_Delete(cJSON *pjson);
+extern cJSON *cJSON_Parse(const char *str);
+
+//
 class CJsonMapNd
 {
 public:    
     CJsonMapNd(const char *pStr);
     CJsonMapNd(int nInt);
+	CJsonMapNd(CJsonMapNd & JsonMapNd);
     CJsonMapNd();
     virtual ~CJsonMapNd();
-    bool IsExist();
-    
-//    int SetInt(int nInt){ Data.m_int = nInt;}
-//    int SetStr(const char *pStr){ Data.m_pStr = pStr;}
+	switch_status_t EncodeJsonString(const char* pString);
+	
+    bool IsExist(const char *pKey);
+	//bool IsExist();
+	int Type();
+	const char* TypeString();
+    int Size();
+	void SetSize(int nNums); // 设置数组列表的大小	
+  	int SetInt(int nInt);
+    int SetStr(const char *pStr);
     const char *Str();
     int Int();
     CJsonMapNd *Obj();
     switch_status_t EncodeJson(cJSON *pJson);
     CJsonMapNd& operator [](const char *pKey);
     CJsonMapNd& operator [](int nIndex); 
+	CJsonMapNd& operator =(const char * pStr);
+	CJsonMapNd& operator =(int nInt);
+	CJsonMapNd& operator =(CJsonMapNd &jsonMapNd);
 private:
+	void Filling(CJsonMapNd &jsonMapNd);
+	void Clear();
     union
     {
-        const char *m_pStr;
+		string *m_pStr;
         int m_int;  
         map<string, CJsonMapNd *> *m_pMap;    // json <key ,value>
         struct
@@ -35,9 +83,14 @@ private:
             CJsonMapNd * pArrayObj;
             int nCount;
         }ARRAY;   //json array
+
+		vector<CJsonMapNd *> *m_pList;
     }Data;
-    
+	
+	
     int m_nType;
+
+	static const char *m_pTypeString[cJSON_TypeMAX];
 };
 
 class CJsonMap
@@ -46,6 +99,8 @@ public:
     virtual ~CJsonMap();
     switch_status_t EncodeJsonString(const char* pString);
     CJsonMapNd& operator [](const char *pKey);
+	bool IsExist(const char *pKey);
+	//bool IsExist();
 private:
     CJsonMapNd m_jsonmapNode;
     cJSON *m_pjson;
